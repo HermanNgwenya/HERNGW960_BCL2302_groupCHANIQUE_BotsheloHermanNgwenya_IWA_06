8b1c42f9a0e1f7304e5d2e45727e8a4bd3c974bb
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-04 15:41:16 +0200

    IWA_06

--- Challenge 2/scripts.js
+++ Challenge 2/scripts.js
@@ -1,3 +1,4 @@
+/*
 const rent = 400;
 const tax = '12%';
 const food = 51.7501;
@@ -15,3 +16,23 @@ if (hourOfDay && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00
 }
 	
 console.log(balance.toFixed(3))
+*/
+
+const rent = 400;
+const tax = '12%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 0;
+const minuteOfDay = 0;
+
+// Only change below this line
+
+if (hourOfDay == 0 && minuteOfDay == 0){
+	let taxAsDecimal = parseInt(tax)/ 100;
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent;
+    console.log(balance.toFixed(2));
+}
+
+	
