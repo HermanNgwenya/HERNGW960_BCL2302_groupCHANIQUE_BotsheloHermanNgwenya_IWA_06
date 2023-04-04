2ca7885afafcad57049824c32e4c53e68f1d23ae
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-04 14:32:54 +0200

    IWA_06

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -3,8 +3,24 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number String == primaryPhone 
-const secondaryValid = typeof number String == secondaryPhone
+const primaryValid = typeof primaryPhone == "number";
+const secondaryValid = typeof secondaryPhone == "string";
+
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid );
+
+
+
+
+/*
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = typeof number == primaryPhone 
+const secondaryValid = typeof number == secondaryPhone
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
+*/
