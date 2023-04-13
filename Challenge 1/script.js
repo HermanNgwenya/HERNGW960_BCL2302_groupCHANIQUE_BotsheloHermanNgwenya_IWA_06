7546b8192290ff2bfbaf8e4a9b8266adf81a5aeb
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-13 13:05:04 +0200

    Interactive-Web-Apps

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof primaryPhone == "number";
-const secondaryValid = typeof secondaryPhone == "string";
+const primaryValid = typeof primaryPhone == "string" && !isNaN(primaryPhone);
+const secondaryValid = typeof secondaryPhone == "string" && !isNaN(secondaryPhone);
 
 console.log('Primary phone is valid numerical string:', primaryValid);
 console.log('Secondary phone is valid numerical string:', secondaryValid );
--- Challenge 2/scripts.js
+++ Challenge 2/scripts.js
@@ -18,8 +18,13 @@ if (hourOfDay && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00
 console.log(balance.toFixed(3))
 */
 
+
+//solution
+
+
+
 const rent = 400;
-const tax = '12%';
+const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -29,10 +34,11 @@ const minuteOfDay = 0;
 // Only change below this line
 
 if (hourOfDay == 0 && minuteOfDay == 0){
-	let taxAsDecimal = parseInt(tax)/ 100
-    const startingAfterTax = salary * (1 - taxAsDecimal)
-	const balance = startingAfterTax - transport - food - rent
-    console.log(balance.toFixed(2))
+	let taxAsDecimal = parseInt(tax)/ 100;
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent;
+	let currency = 'R';
+    console.log(currency , balance.toFixed(2));
 }
 
 	
